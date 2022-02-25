a1f67802cbc41f790ea2a3664c900f1115a05b90
CristianPere <cristianperezcastillo.98@gmail.com>
2022-02-25 09:38:00 +0100

    finalExamen

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('propietarios', PropietarioController::class);
 
+//Route::middleware('auth:sanctum')->get('propietarios/', [CursoController::class]);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
